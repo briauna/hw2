086d52fe50494b2c40ca7e92c27488efef706052
briauna <31833842+briauna@users.noreply.github.com>
2017-09-15 14:45:14 -0700

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,30 @@
+
+while (getColor() != "red") {
+  down();
+}
+function updown() {
+  if (getColor() == "red") {
+    right();
+    right();
+  }
+}
+while (getColor() != "red") {
+  up();
+}
+updown();
+while (getColor() != "red") {
+  up();
+}
+updown();
+while (getColor() != "red") {
+  down();
+}
+updown();
+while (getColor() != "red") {
+  up();
+}
+updown();
+while (getColor() != "red") {
+  down();
+}
+updown();
