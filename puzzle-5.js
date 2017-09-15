916ab3eec01a4c7ae507fc240791fb9c98ebcd92
briauna <31833842+briauna@users.noreply.github.com>
2017-09-15 14:30:38 -0700

    Create puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,20 @@
+function updown() {
+  if (getColor() == "blue") {
+    down();
+    right();
+  }
+  else
+    if (getColor() != "blue") {
+      up();
+      right();
+    }
+}
+right();
+right();
+updown();
+right();
+updown();
+right();
+updown();
+right();
+right();
