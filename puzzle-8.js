74d54a874532a17e7588423d7a609394e7588509
briauna <31833842+briauna@users.noreply.github.com>
2017-09-15 15:16:28 -0700

    Create puzzle-8.js

---
+++ puzzle-8.js
@@ -0,0 +1,35 @@
+var steps = 0;
+while (steps <= 8) {
+  down();
+  steps = steps + 1;
+}
+right();
+right();
+var steps = 0;
+while (steps < 8) {
+  up();
+  steps = steps + 1;
+}
+right();
+right();
+var steps = 0;
+while (steps < 8) {
+  down();
+  steps = steps + 1;
+}
+right();
+right();
+var steps = 0;
+while (steps < 8) {
+  up();
+  steps = steps + 1;
+}
+right();
+right();
+var steps = 0;
+while (steps < 8) {
+  down();
+  steps = steps + 1;
+}
+right();
+up();
